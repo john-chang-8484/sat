fa13d89d00fdf1798a27aa4eef742bff4c2bbdba
Phillip B <phillipb163@gmail.com>
2019-08-12 22:09:46 +0100

    rule 110 success

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from makecnf import makecnf, parse_result, solve
 from cnfutil import n, Clauses
 
 
+width = 14
+duration = 12
+
+
+def deparen(l):
+    """ strip away all layers of enclosing lists """
+    if isinstance(l, list) and len(l) == 1:
+        return deparen(l[0])
+    return l
+
+
 def indexify(nlist, ind):
     """ put indices on all variables with trailing underscores
         assumes the only mutable elements of nlist are lists
@@ -10,28 +21,82 @@ def indexify(nlist, ind):
     if isinstance(nlist, list):
         return [indexify(sub, ind) for sub in nlist]
     if isinstance(nlist, str) and nlist[-1] == '_':
-        return nlist + '~' + str(ind)
+        return nlist + ind
     return nlist
 
+def at(v, t, x):
+    """ value of a field at some location in space """
+    return v + '_%d_%d' % (t % duration, x % width)
+
+def pos(v):
+    """ get the position in space of some field variable """
+    return tuple(map(int, v.split('_')[1:]))
+
+def nm(v):
+    """ get the field name of some field variable """
+    return v.split('_')[0]
+
+def right(v):
+    """ returns the field variable just right of v """
+    name, (t, x) = nm(v), pos(v)
+    return at(name, t, x + 1)
+
+def left(v):
+    """ returns the field variable just left of v """
+    name, (t, x) = nm(v), pos(v)
+    return at(name, t, x - 1)
+
+def forward(v):
+    """ returns the field variable just futurewards of v """
+    name, (t, x) = nm(v), pos(v)
+    return at(name, t + 1, x )
+
+def back(v):
+    """ returns the field variable just pastwards of v """
+    name, (t, x) = nm(v), pos(v)
+    return at(name, t - 1, x )
 
 def main():
     c = Clauses()
     
+    ######### Macro Definitions ##########
+    def mac_at(args):
+        return at(deparen(args[0]), deparen(args[1]), deparen(args[2]))
+    c.addmacro('at', mac_at)
+    
     def forall(args):
         assert len(args) == 1
-        return ['&', *[indexify(args[0], i) for i in range(0, 10)]]
-    
+        ans = ['&']
+        for t in range(0, duration):
+            for x in range(0, width):
+                ans.append(indexify(args[0], '%d_%d' % (t, x)))
+        return ans
     c.addmacro('forall', forall)
-    #c.defmacro('and', ['a', 'b'], '(& a b)')
-    c.run('(forall (& a_ b_))')
-    c.print_clauses()
+    
+    c.defmacro('r110', ['p', 'q', 'r'], '(| (& q (~ p)) (^ q r))')
+    
+    c.addmacro('forward', (lambda args: forward(args[0][0])))
+    c.addmacro('back', (lambda args: back(args[0][0])))
+    c.addmacro('left', (lambda args: left(args[0][0])))
+    c.addmacro('right', (lambda args: right(args[0][0])))
+    
+    ###### End of Macro Definitions ######
+    
+    c.run('(forall (= (forward cell_) (r110 (left cell_) cell_ (right cell_))))')
+    c.run('(at cell 0 0)')
+    # c.print_clauses()
 
     solution = solve(c.get_clauses())
     if not solution:
         print('UNSAT')
     else:
         print('SAT')
-        print(solution)
+        #print(solution)
+        for t in range(0, duration):
+            line = '| '
+            for x in range(0, width):
+                line += '# ' if solution[at('cell', t, x)] else '  '
+            print(line + '|')
     
 
 if __name__ == '__main__':
